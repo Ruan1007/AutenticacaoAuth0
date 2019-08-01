84553637d3b800708892abf32364e9ea61ac3001
Ruan Barbosa <ruan.barbosa@squadra.com.br>
2019-08-01 15:33:32 -0300

    first commit

---
+++ src/main/java/com/southamericadevelopers/AutenticacaoAuth0/AutenticacaoAuth0Application.java
@@ -0,0 +1,13 @@
+package com.southamericadevelopers.AutenticacaoAuth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacaoAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacaoAuth0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/southamericadevelopers/AutenticacaoAuth0/AutenticacaoAuth0ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.southamericadevelopers.AutenticacaoAuth0;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutenticacaoAuth0ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
